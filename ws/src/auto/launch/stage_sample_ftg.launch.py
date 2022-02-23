de89ac1dbac401170fde8f6b740294b283a5717e
Martin Endler <pokusew@seznam.cz>
2022-02-22 20:39:49 -0500

    [WIP] Google Cartograhper SLAM

--- ws/src/auto/launch/stage_sample_ftg.launch.py
+++ ws/src/auto/launch/stage_sample_ftg.launch.py
@@ -1,92 +1,167 @@
-import launch
-import launch.launch_description_sources
-import launch_ros
+import os
+from typing import List
+
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription, SomeSubstitutionsType
+from launch.actions import DeclareLaunchArgument, SetLaunchConfiguration, LogInfo, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, ThisLaunchFileDir
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def get_shared_file(package_name: str, *path: List[str]) -> str:
+    """Return the path to the given shared file."""
+    return os.path.join(
+        get_package_share_directory(package_name),
+        *path,
+    )
+
+
+def get_shared_file_substitution(package_name: str, *path: List[str]) -> SomeSubstitutionsType:
+    """Return a Substitution that resolves to the path to the given shared file."""
+    return PathJoinSubstitution([
+        FindPackageShare(package=package_name),
+        *path,
+    ])
 
 
 def generate_launch_description():
-    return launch.LaunchDescription([
 
-        launch.actions.DeclareLaunchArgument(
-            name='world',
-            default_value='test_track',
-            description='Name of a world within storage/stage/world',
+    stage_launch = IncludeLaunchDescription(
+        launch_description_source=PythonLaunchDescriptionSource(
+            launch_file_path=PathJoinSubstitution([
+                ThisLaunchFileDir(),
+                'stage.launch.py',
+            ]),
         ),
+        launch_arguments=[
+            ('world', LaunchConfiguration(variable_name='world')),
+        ],
+    )
 
-        # TODO: rviz
-        # TODO: map
+    # TODO: refactor (declarative way, avoid side effects, use dynamic substition)
+    urdf_path = get_shared_file('cartographer_slam', 'urdf', 'tx2-auto-3.urdf')
+    with open(urdf_path, 'r') as urdf_file_handle:
+        urdf_file_content = urdf_file_handle.read()
 
-        launch.actions.IncludeLaunchDescription(
-            launch_description_source=launch.launch_description_sources.PythonLaunchDescriptionSource(
-                launch_file_path=launch.substitutions.PathJoinSubstitution([
-                    launch.substitutions.ThisLaunchFileDir(),
-                    'stage.launch.py'
-                ]),
+    urdf_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        parameters=[
+            {
+                # Parameter robot_description as file path, not URDF string
+                # https://github.com/ros/robot_state_publisher/issues/155
+                'robot_description': urdf_file_content,
+                'use_sim_time': True,
+            },
+        ],
+    )
+
+    # see https://docs.ros.org/en/rolling/Tutorials/Tf2/Writing-A-Tf2-Static-Broadcaster-Py.html#the-proper-way-to-publish-static-transforms
+    tf_stage_base = Node(
+        executable='static_transform_publisher',
+        name='tf_stage_base',
+        package='tf2_ros',
+        output='screen',
+        arguments=[
+            # x y z qx qy qz qw
+            '0', '0', '0', '0', '0', '0',
+            # frame_id child_frame_id
+            'car/base_link', 'base_link',
+        ],
+    )
+    tf_stage_laser = Node(
+        executable='static_transform_publisher',
+        name='tf_stage_laser',
+        package='tf2_ros',
+        output='screen',
+        arguments=[
+            # x y z qx qy qz qw
+            '0', '0', '0', '0', '0', '0',
+            # frame_id child_frame_id
+            'laser', 'car/ranger_0/base_scan',
+        ],
+    )
+
+    obstacle_substitution_node = Node(
+        package='obstacle_substitution',
+        executable='obstacle_substitution_node',
+        name='obstacle_substitution_node',
+        remappings=[
+            # ('/scan', ''),
+            # ('/obstacles', ''),
+        ],
+        parameters=[
+            {
+                'use_sim_time': True,
+            },
+        ],
+        output='screen',
+    )
+
+    follow_the_gap_v0_ride_launch = IncludeLaunchDescription(
+        launch_description_source=PythonLaunchDescriptionSource(
+            launch_file_path=get_shared_file_substitution(
+                'follow_the_gap_v0_ride', 'start.launch.py',
             ),
-            launch_arguments=[
-                ('world', launch.substitutions.LaunchConfiguration(variable_name='world')),
-            ],
+        ),
+        launch_arguments=[],
+    )
+
+    drive_api_node = Node(
+        package='drive_api',
+        executable='drive_api_node',
+        name='drive_api_sim_node',
+        parameters=[
+            get_shared_file_substitution(
+                'drive_api', 'config', 'drive_api_node.yaml',
+            ),
+            {
+                'run_mode': 'simulation',
+                'simulation.steering_modifier': 0.7,
+                'simulation.throttle_modifier': 7.26,
+            },
+            {
+                'use_sim_time': True,
+            },
+        ],
+        remappings=[
+            ('/cmd_vel', '/stage_ros2/car/cmd_vel'),
+        ],
+        output='screen',
+    )
+
+    return LaunchDescription([
+
+        DeclareLaunchArgument(
+            name='world',
+            description='World file name (incl. ext). Can be a relative path (with respect to storage/stage/world) or an absolute path.',
+            default_value='test_track.world',
         ),
 
+        stage_launch,
+        # urdf_publisher,
+        # tf_stage_base,
+        # tf_stage_laser,
         # TODO: Is it needed with stage_ros2?
         # <!-- Transformation -->
         # <node
         # 	pkg="tf" type="static_transform_publisher"
         # 	name="tf_base_laser_link"
-        # 	args="0 0 0 0 0 0 base_laser_link /robot_0/base_laser_link 100"
+        # 	args="0 0 0 0 0 0 base_laser_link car/ranger_0/base_scan"
         # />
         # <node
         # 	pkg="tf"
         # 	type="static_transform_publisher"
         # 	name="tf_odom_map"
-        # 	args="0 0 0 0 0 0 odom map 100"
+        # 	args="0 0 0 0 0 0 odom map"
         # />
-
-        # obstacle_substitution
-        launch.actions.IncludeLaunchDescription(
-            launch_description_source=launch.launch_description_sources.PythonLaunchDescriptionSource(
-                launch_file_path=launch.substitutions.PathJoinSubstitution([
-                    launch_ros.substitutions.FindPackageShare(package='obstacle_substitution'),
-                    'start.launch.py'
-                ]),
-            ),
-            launch_arguments=[
-                ('remap', 'true'),
-                ('/scan', '/stage_ros2/car/ranger_0'),
-            ],
-        ),
-
-        # follow_the_gap_v0_ride
-        launch.actions.IncludeLaunchDescription(
-            launch_description_source=launch.launch_description_sources.PythonLaunchDescriptionSource(
-                launch_file_path=launch.substitutions.PathJoinSubstitution([
-                    launch_ros.substitutions.FindPackageShare(package='follow_the_gap_v0_ride'),
-                    'start.launch.py'
-                ]),
-            ),
-            launch_arguments=[],
-        ),
-
-        # drive_api
-        launch_ros.actions.Node(
-            package='drive_api',
-            executable='drive_api_node',
-            output='screen',
-            name=launch.substitutions.AnonName(name='drive_api'),
-            parameters=[
-                launch.substitutions.PathJoinSubstitution([
-                    launch_ros.substitutions.FindPackageShare(package='drive_api'),
-                    'config',
-                    'drive_api_node.yaml'
-                ]),
-                {
-                    'run_mode': 'simulation',
-                    'simulation.steering_modifier': 0.7,
-                    'simulation.throttle_modifier': 7.26,
-                },
-            ],
-            remappings=[
-                ('/cmd_vel', '/stage_ros2/car/cmd_vel'),
-            ],
-        ),
+        # TODO: rviz
+        # TODO: map
+        obstacle_substitution_node,
+        follow_the_gap_v0_ride_launch,
+        drive_api_node,
 
     ])
---
+++ ws/src/perception/recognition/cartographer_slam/launch/localization.launch.py
@@ -0,0 +1,148 @@
+"""TODO"""
+
+import os
+from typing import List
+
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription, SomeSubstitutionsType
+from launch.actions import DeclareLaunchArgument, SetLaunchConfiguration, LogInfo
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def get_shared_file(package_name: str, *path: List[str]) -> str:
+    """Return the path to the given shared file."""
+    return os.path.join(
+        get_package_share_directory(package_name),
+        *path,
+    )
+
+
+def get_shared_file_substitution(package_name: str, *path: List[str]) -> SomeSubstitutionsType:
+    """Return a Substitution that resolves to the path to the given shared file."""
+    return PathJoinSubstitution([
+        FindPackageShare(package=package_name),
+        *path,
+    ])
+
+
+THIS_PKG = 'cartographer_slam'
+
+
+def generate_launch_description():
+
+    # https://github.com/ros2/cartographer_ros
+    cartographer_node = Node(
+        executable='cartographer_node',
+        name='cartographer_node',
+        namespace='cartographer',
+        package='cartographer_ros',
+        output='screen',
+        parameters=[
+            {
+                'use_sim_time': True,
+            },
+        ],
+        arguments=[
+            '-configuration_directory',
+            get_shared_file_substitution(THIS_PKG, 'config'),
+            '-configuration_basename',
+            'localization.lua',
+            '-load_state_filename',
+            LaunchConfiguration(variable_name='map'),
+        ],
+        remappings=[
+            # see https://google-cartographer-ros.readthedocs.io/en/latest/ros_api.html
+            # ('echoes', 'horizontal_laser_2d'),
+            ('scan', '/scan'),
+            # ('/imu', 'xxx'),
+        ],
+    )
+
+    occupancy_grid_node = Node(
+        executable='occupancy_grid_node',
+        name='occupancy_grid_node',
+        namespace='cartographer',
+        package='cartographer_ros',
+        output='screen',
+        arguments=[
+            '-resolution',
+            '0.05',
+        ],
+        parameters=[
+            {
+                'use_sim_time': True,
+            },
+        ],
+    )
+
+    # see https://docs.ros.org/en/rolling/Tutorials/Tf2/Writing-A-Tf2-Static-Broadcaster-Py.html#the-proper-way-to-publish-static-transforms
+    static_transform_publisher_laser = Node(
+        executable='static_transform_publisher',
+        name='static_transform_publisher_laser',
+        package='tf2_ros',
+        output='screen',
+        arguments=[
+            # x y z qx qy qz qw
+            '0', '0', '0', '0', '0', '0',
+            # frame_id child_frame_id
+            'base_link', 'laser',
+        ],
+    )
+
+    # see https://docs.ros.org/en/rolling/Tutorials/Tf2/Writing-A-Tf2-Static-Broadcaster-Py.html#the-proper-way-to-publish-static-transforms
+    static_transform_publisher_imu = Node(
+        executable='static_transform_publisher',
+        name='static_transform_publisher_imu',
+        package='tf2_ros',
+        output='screen',
+        arguments=[
+            # x y z qx qy qz qw
+            '0', '0', '0', '0', '-0.707', '0.707',
+            # frame_id child_frame_id
+            'base_link', 'base_imu_link',
+        ],
+    )
+
+    # TODO: refactor (declarative way, avoid side effects, use dynamic substition)
+    urdf_path = get_shared_file(THIS_PKG, 'urdf', 'tx2-auto-3.urdf')
+    with open(urdf_path, 'r') as urdf_file_handle:
+        urdf_file_content = urdf_file_handle.read()
+
+    urdf_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        parameters=[
+            {
+                # Parameter robot_description as file path, not URDF string
+                # https://github.com/ros/robot_state_publisher/issues/155
+                'robot_description': urdf_file_content,
+            },
+        ],
+    )
+
+    return LaunchDescription([
+
+        DeclareLaunchArgument(
+            name='map',
+            description='Map file name (incl. ext). Can be a relative path (with respect to storage/stage/world) or an absolute path.',
+        ),
+
+        SetLaunchConfiguration(
+            name='map',
+            value=get_shared_file_substitution(
+                'storage', 'stage', 'world',
+                LaunchConfiguration(variable_name='map')
+            ),
+        ),
+
+        cartographer_node,
+        occupancy_grid_node,
+
+        # urdf_publisher,
+        # static_transform_publisher_laser,
+        # static_transform_publisher_imu,
+
+    ])
