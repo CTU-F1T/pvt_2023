f72fcd9e40f18de1e75e7464748472ced82d77fa
Martin Endler <pokusew@seznam.cz>
2022-06-24 23:20:18 -0400

    Move ws dir contents to the project root

--- patches/patch-stage.sh
+++ patches/patch-stage.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-git apply --directory ws patches/Fix_stage_CMakeLists_txt_Fix_stage_ros2_CMakeLists_txt.patch
+git apply patches/Fix_stage_CMakeLists_txt_Fix_stage_ros2_CMakeLists_txt.patch
+# TODO: include the changes in to the patch file
+cp patches/stage_ros2_CMakeLists.txt src/simulation/stage_ros2/CMakeLists.txt
