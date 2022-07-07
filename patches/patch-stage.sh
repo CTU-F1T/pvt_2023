a829d3f594c98d303178343e9f45ec5e953a53e5
Martin Endler <pokusew@seznam.cz>
2022-07-06 23:41:51 -0400

    Update patches

--- patches/patch-stage.sh
+++ patches/patch-stage.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
 git apply patches/Fix_stage_CMakeLists_txt_Fix_stage_ros2_CMakeLists_txt.patch
-# TODO: include the changes in to the patch file
-cp patches/stage_ros2_CMakeLists.txt src/simulation/stage_ros2/CMakeLists.txt
