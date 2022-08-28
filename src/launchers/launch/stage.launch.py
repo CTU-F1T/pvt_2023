d00c6245ae8f832624505ac5379cf927323a7c01
Martin Endler <pokusew@seznam.cz>
2022-08-28 08:17:50 +0200

    Remove obsolete launchers package (merged into auto package)

--- src/launchers/launch/stage.launch.py
+++
@@ -1,8 +0,0 @@
-# NOTE: this is workaround for misbehaviour of symlink install
-#       see the project README.md (the top-level one)
-#       for more info (section [Launch files are not symlinked])
-from launchers import launchers
-
-
-def generate_launch_description():
-    return launchers.generate_stage_launch_description()
