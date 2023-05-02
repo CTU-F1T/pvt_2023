03b9ecceb1da0ac3ac25eec1675b1d68bbe7237f
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2023-05-02 06:46:28 +0200

    [auto-cli] Publish start/stop messages with '-w 0'
    For some reason the default value for '-1' is 1 (i.e., wait for
    one subscriber before publishing the message), which for some ROS2
    whatever reason leads to sending the message to one node only.
    
    It seems that setting '-w 0' solves this.

--- src/auto/scripts/auto-cli/plugins/start.sh
+++ src/auto/scripts/auto-cli/plugins/start.sh
@@ -2,4 +2,4 @@
 
 set -ex
 
-ros2 topic pub /eStop -1 std_msgs/msg/Bool 'data: False'
+ros2 topic pub /eStop -1 -w 0 std_msgs/msg/Bool 'data: False'
--- src/auto/scripts/auto-cli/plugins/stop.sh
+++ src/auto/scripts/auto-cli/plugins/stop.sh
@@ -2,4 +2,4 @@
 
 set -ex
 
-ros2 topic pub /eStop -1 std_msgs/msg/Bool 'data: True'
+ros2 topic pub /eStop -1 -w 0 std_msgs/msg/Bool 'data: True'
